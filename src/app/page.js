9f3e7106887749d00f338cf3768fcbe4ca8da695
harshraj438524 <21bec050@iiitdmj.ac.in>
2023-12-19 12:48:27 +0530

    body section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
-import HomeHeader from '@/components/HomeHeader'
-import Image from 'next/image'
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 export default function Home() {
   return (
-      <>
-         <HomeHeader/>
-      </>
-  )
+    <>
+      <HomeHeader />
+
+      {/* body of home page */}
+
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="/google-image2.png"
+        />
+        <HomeSearch/>
+      </div>
+    </>
+  );
 }
